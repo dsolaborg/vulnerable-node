f85bf4b2c81ab2315f93acbb7d7278b495ca8908
Clyde Fondop <cfsbarrow@gmail.com>
2018-11-13 16:51:16 -0500

    Update main.tf
    forward http to https

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_lb_listener" "front_end_https" {
 
 resource "aws_lb_listener" "front_end_http" {
   load_balancer_arn = "${aws_lb.front_end.arn}"
-  port              = "3000"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
